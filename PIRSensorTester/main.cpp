80fd75709d49ccf62a9cdcb1aa41fd5d0b9a606e
Amit Jain <ajonnet@gmail.com>
2019-09-19 22:55:15 +0530

    Initial Commit

---
+++ PIRSensorTester/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PIRSensorTester
+//
+//  Created by Amit Jain on 19/09/19.
+//  Copyright © 2019 ajonnet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
